a10f566d8712b84af4ac69984529c4faa84fcad9
ShamimHossain1 <beastb697@gmail.com>
2023-01-27 00:01:14 +0600

    array syntex

---
+++ array-practice.js
@@ -0,0 +1,16 @@
+var friendsAge =[11, 23, 45, 67];
+//positions----->0    1   2   3
+
+var naikas = ['mahi', 'opu', 'sabnoor', 'sabana'];
+//positions---->0       1        2          3
+
+var vowels = ["s", "e", "i", "o", "u"];
+//positions--->0    1    2    3    4
+
+console.log(friendsAge);
+console.log(naikas);
+console.log(vowels);
+
+console.log(friendsAge.length);
+console.log(naikas.length);
+console.log(vowels.length);
