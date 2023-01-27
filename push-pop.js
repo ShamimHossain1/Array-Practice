b95011086bff72ddc0ac2abd558af3cc0cd31830
ShamimHossain1 <beastb697@gmail.com>
2023-01-27 16:57:52 +0600

    array push pop

---
+++ push-pop.js
@@ -0,0 +1,26 @@
+// push used to add an element at the last of an Array
+var numbers=[20, 30, 40, 50];
+numbers.push(60);
+// console.log(numbers);
+
+// add an element at the start of an array
+
+var names=['shamim', 'hamim', 'shajim'];
+names.unshift('hasan');
+// console.log(names);
+
+// pop used to remove last element from an Array
+
+var price=[100, 200, 300, 400];
+// console.log(price);
+price.pop();
+console.log(price);
+var prices=price.pop();
+console.log(prices);
+
+// Remove an element at the start of an array
+
+var games=["gta 5", "pubg", "fortnite", "cod"];
+games.shift();
+
+// console.log(games);
